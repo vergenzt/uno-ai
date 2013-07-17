6f667b41d180348b4c62d535d79c73f6ee75bdb4
Tim Vergenz <vergenzt@gmail.com>
2013-07-17 18:05:28 +0200

    Add imports to __init__.py's

---
+++ uno/ai/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on Jul 17, 2013
+
+@author: Tim Vergenz
+'''
+
+from baseagent import UnoAgent
---
+++ uno/game/__init__.py
@@ -0,0 +1,9 @@
+'''
+Created on Jul 17, 2013
+
+@author: Tim Vergenz
+'''
+
+from base import UnoGame
+from deck import UnoDeck, UnoPile, UnoCard
+from deck import Num, Special, Skip, Reverse, DrawTwo
